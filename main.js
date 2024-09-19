bc4ca055e074bf8f6c85ee810c15606a78e03cfa
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 08:14:46 -0400

    Init the hit() and isSunk() functions within ship()

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 function ship(length) {
-  return length;
+  return {
+    length: length,
+    hit() {
+      return (this.length -= 1);
+    },
+    isSunk() {
+      return (this.isSunk = true);
+    },
+  };
 }
 module.exports = ship;
