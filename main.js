8dcedde425643b817b93c52c95279ec9c0627036
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 09:39:30 -0400

    eslint: fix all indent problems

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 class Ship {
-    hits = 0;
-    constructor(length) {
-        this.length = length;
+  hits = 0;
+  constructor(length) {
+    this.length = length;
+  }
+
+  hit() {
+    this.hits++;
+  };
+
+  isSunk() {
+    if (this.length === this.hits) {
+      return this.isSunk = true;
+    } else {
+      return this.isSunk = false;
     }
-    hit() {
-      this.hits++;
-    };
-    isSunk() {
-      if (this.length === this.hits) {
-        return this.isSunk = true;
-      } else {
-        return this.isSunk = false;
-      }
-      
-    };
+  };
 }
 
 module.exports = Ship;
