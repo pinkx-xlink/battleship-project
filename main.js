abdb8be7857dedba6611fcba74e37c84b3fd9c7e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 09:57:19 -0400

    Init the Gameboard class and constructor

--- main.js
+++ main.js
@@ -17,7 +17,13 @@ class Ship {
   };
 }
 
-
+class Gameboard {
+    // takes in the x and y axis (number of squares up and down)
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 
 module.exports = Ship;
