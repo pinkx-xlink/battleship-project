5f29f457d698b268adca183dccdcfe7af80686d3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 12:07:03 -0400

    Add a for loop that creates the gameboard based on user input

--- gameboard.js
+++ gameboard.js
@@ -4,7 +4,18 @@ class Gameboard {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        for(let i = 0; i < x; i++) {
+            // const tr = document.createElement('tr');
+            // let cellRowCoord = Math.abs(x - 7);
+            // tr.textContent = cellRowCoord;
+            for (let j = 0; j < y; j++) {
+            //   const cell = document.createElement('td');
+            //   let cellColumnCoord = y;
+            //   cell.textContent = cellColumnCoord;
+            }
+        }
     }
+
 
     logBoard() {
         return this.x, this.y;
