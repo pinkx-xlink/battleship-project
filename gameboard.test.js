3c42d224c0f740a49088a439cdd445395ab6b744
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 10:39:18 -0400

    Create myBoard - test passes

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,5 +1,5 @@
 const gameboard = require('./gameboard');
-
+const myBoard = new gameboard(10, 10);
 test('gameboard measures 10 x 10', () => {
-    expect(gameboard(10, 10).logBoard()).toBe(10, 10)
+    expect(myBoard.logBoard()).toBe(10, 10)
   });
