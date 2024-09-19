f89ad980a69bf97db55d5edecb50227798d963da
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 09:37:14 -0400

    Write test for if ship is sunk - fail

--- main.test.js
+++ main.test.js
@@ -14,3 +14,11 @@ test('ship1 takes a hit', () => {
   ship1.hit();
   expect(ship1.hits).toBe(1);
 });
+
+// tests to see if the ship has sunk
+test('ship2 is sunk', () => {
+    const ship2 = new ship(2);
+    ship2.hit();
+    ship2.hit();
+    expect(ship2.isSunk()).toBe(true);
+  });
