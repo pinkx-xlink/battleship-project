144534bd3b3d2aa955cddd6f3bf6ae0ccaa55a32
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-19 08:11:49 -0400

    Refactor example function, turn it into ship() factory functio and run the first test on it (pass)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
+function ship(length) {
+  return length;
+}
+module.exports = ship;
--- main.test.js
+++ main.test.js
@@ -1,5 +1,5 @@
-const sum = require('./main');
+const ship = require('./main');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('gets the length of the ship', () => {
+  expect(ship(4)).toBe(4);
 });
