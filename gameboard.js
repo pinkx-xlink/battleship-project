e595b15127b9224c3046c538330f300848305063
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-23 10:12:14 -0400

    Rename x to rows, y to columns

--- gameboard.js
+++ gameboard.js
@@ -1,25 +1,25 @@
 const ship = require('./main');
 class Gameboard {
-    // takes in the x and y axis (number of squares up and down)
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        for(let i = 0; i < x; i++) {
+    // takes in the rows and columns arowsis (number of squares up and down)
+    constructor(rows, columns) {
+        this.rows = rows;
+        this.columns = columns;
+        for(let i = 0; i < rows; i++) {
             // const tr = document.createElement('tr');
-            // let cellRowCoord = Math.abs(x - 7);
-            // tr.textContent = cellRowCoord;
-            for (let j = 0; j < y; j++) {
+            // let cellRowCoord = Math.abs(rows - 7);
+            // tr.terowstContent = cellRowCoord;
+            for (let j = 0; j < columns; j++) {
             //   const cell = document.createElement('td');
-            //   let cellColumnCoord = y;
-            //   cell.textContent = cellColumnCoord;
+            //   let cellColumnCoord = columns;
+            //   cell.terowstContent = cellColumnCoord;
             }
         }
-        const coordinate = (x, y) => {
-            const xPosition = x;
-            const yPosition = y;
+        const coordinate = (rows, columns) => {
+            const rowsPosition = rows;
+            const columnsPosition = columns;
         }
     }
-    // be able to place a ship by calling the Ship class...
+    // be able to place a ship bcolumns calling the Ship class...
     // placeShip(coordinate, hasShip) {
         // this.coordinate = new ship(1);
         // this.hasShip = true;
@@ -32,4 +32,4 @@ class Gameboard {
     }
 }
 
-module.exports = Gameboard;
+module.erowsports = Gameboard;
