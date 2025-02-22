3f5c5e5be29eb3a9f78f1635138c7be2dba9221a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-21 20:59:29 -0500

    Write a new test

--- main.test.js
+++ main.test.js
@@ -1,12 +1,15 @@
 const Ship = require('./main');
 const ships = require('./main');
+jest.mock('./main');
 
+const { describe, it, expect } = require('@jest/globals');
 
 //let ship1 = new Ship('carrier', 5);
 test('make a new ship', () => {
   const newShip = new Ship('destroyer', 1);
-  expect(newShip.name).toBe('destroyer');
-  expect(newShip.length).toBe(1);
+  expect(Ship).toHaveBeenCalledTimes(1)
+  // expect(newShip.name).toBe('destroyer');
+  // expect(newShip.length).toBe(1);
 });
 
 // ship1 takes a hit()
@@ -17,12 +20,16 @@ test('make a new ship', () => {
 
 // tests to see if the ship has sunk
 test('ship2 is sunk', () => {
-    const ship2 = new Ship('baby', 2);
-    ship2.hit();
-    ship2.hit();
-    expect(ship2.isSunk()).toBe(true);
-  });
+  const ship2 = new Ship('baby', 2);
+  ship2.hit();
+  ship2.hit();
+  expect(ship2.isSunk()).toBe(true);
+});
 
-  test('log all the different types of ships', () => {
-    expect(ships).toBe([destroyer, submarine, cruiser, battleship, carrier]);
-  })
+test('should log carrier info', () => {
+  expect(console.log(carrier).toBe('carrier', 5))
+})
+
+// test('log all the different types of ships', () => {
+//   expect(console.log(ships)).toBe(["destroyer, submarine, cruiser, battleship, carrier"]);
+// })
